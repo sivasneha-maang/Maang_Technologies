f64302f04c70fda32261a270adfacbd0e635096c
Katteragandla.Siva Sneha <157129473+sneha654321@users.noreply.github.com>
2024-01-30 22:18:08 +0530

    Add files via upload

---
+++ JAVASCRIPT/Add_2numbers.js
@@ -0,0 +1,12 @@
+
+// js function for addition of two numbers.
+
+function add(a,b)
+{
+	return a+b;
+}
+
+let a=10;
+let b=20;
+l
+document.write(add(a,b));
---
+++ JAVASCRIPT/square.js
@@ -0,0 +1,11 @@
+// js function for squaring the numberr.
+
+function square(n)
+{
+	ans=n*n;
+	return ans;
+}
+
+let number=102;
+result=square(number);
+console.log(result);
